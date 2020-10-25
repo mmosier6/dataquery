1341dc9900fc98df0d8c542e452edb01353b626d
mmosier6 <mosier.matt@gmail.com>
2020-10-25 04:19:13 -0500

    Initial commit for web branch

--- index.php
+++ index.php
@@ -1,23 +1,23 @@
-<?php 
+<?php
 	if(isset($_GET["date"])){
 		$d = $_GET["date"];
 	}
-	
+
 	if(isset($_GET["debug"])){
 		$debug = 1;
 	}else{
 		$debug = 0;
 	}
-	
+
 	if(isset($_GET["winter"])){
 		$winter = True;
 	}
-	
+
 	if(isset($_GET["all"])){
 		$all = True;
 	}
 
- 
+
 #######################################################
 #
 # Basic SPC WebPage Template
@@ -27,20 +27,24 @@
 # Page Title
 $pageTitle = "Experimental Data Query Page";
 
-$srcRoot = $_SERVER['DOCUMENT_ROOT']; 
-if ($srcRoot == "") $srcRoot = "/local/spcwebsite";
+$srcRoot = $_SERVER['DOCUMENT_ROOT'];
+if ($srcRoot == "") $srcRoot = getcwd()."/web/";
+$srcRoot = getcwd()."/web/";
+
 require("${srcRoot}/cwi/SPCCorporateWebImage-min.php");
-showHeader($pageTitle, $srcRoot); 
+showHeader($pageTitle, $srcRoot);
 ?>
 
 <!-- ############################################################################## -->
 <!-- ######################### BEGIN MAIN PAGE CONTENT ############################ -->
 <br><br>
-<?php 
-	include 'page.html';	
+<?php
+	include 'page.html';
 ?>
 <br><br>
 <!-- ######################### END MAIN PAGE CONTENT ############################ -->
 <!-- ############################################################################## -->
 
-<?php showFooter($srcRoot); ?>
+<?php
+	showFooter($srcRoot);
+?>
